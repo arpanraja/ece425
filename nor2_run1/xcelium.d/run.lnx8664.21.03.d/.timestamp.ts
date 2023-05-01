2b6a6f1c8a0b2f0cec350b6df9105bcf57fbebf4
arpanr2 <arpanr2@illinois.edu>
2023-05-01 12:12:01 -0500

    starting

---
+++ nor2_run1/xcelium.d/run.lnx8664.21.03.d/.timestamp.ts
@@ -0,0 +1,5 @@
+1139589189 /software/ncsu-cdk-1.6.0.beta/lib/NCSU_Analog_Parts/nmos/functional/verilog.v
+1677534652 /home/arpanr2/ece425.work/nor2_run1/hdlFilesDir/cds_globals.v
+1139589175 /software/ncsu-cdk-1.6.0.beta/lib/NCSU_Analog_Parts/pmos/functional/verilog.v
+1677534652 /home/arpanr2/ece425.work/nor2_run1/ihnl/cds0/netlist
+1677534652 /home/arpanr2/ece425.work/nor2_run1/testfixture.template
---
+++ xor2_run1/xcelium.d/run.lnx8664.21.03.d/files.ts
@@ -0,0 +1,5 @@
+1677536331 /home/arpanr2/ece425.work/xor2_run1/testfixture.template
+1139589175 /software/ncsu-cdk-1.6.0.beta/lib/NCSU_Analog_Parts/pmos/functional/verilog.v
+1139589189 /software/ncsu-cdk-1.6.0.beta/lib/NCSU_Analog_Parts/nmos/functional/verilog.v
+1677536331 /home/arpanr2/ece425.work/xor2_run1/ihnl/cds0/netlist
+1677536331 /home/arpanr2/ece425.work/xor2_run1/hdlFilesDir/cds_globals.v
